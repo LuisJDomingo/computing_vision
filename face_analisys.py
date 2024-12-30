ccd65f037c3cdcb7d08da9651b84689ee55b89b8
Luis J Domingo <luisdomingogarcia79@gmail.com>
2024-12-30 18:02:20 +0100

    face analisys, primera version. usando dlib

--- face_analisys.py
+++ face_analisys.py
@@ -1,17 +1,18 @@
 import cv2
 import face_recognition
 import os
+import imutils
 
 '''coge las imagenes de la carpeta faces
     las examina una por una.
     dentro de un bucle for in
 '''
 print("--------------------------")
-img = cv2.imread(f"computing_vision\faces\face_0.jpg")
+img = cv2.imread(r"computing_vision\faces\face_0.jpg")
 if img is None:
-    raise ValueError(f"Error al cargar la imagen: {image_path}")
-
+    print(f"Error al cargar la imagen: {img}")
 face_loc = face_recognition.face_locations(img)
+print("face_loc: ", face_loc)
 face_image_encodings = face_recognition.face_encodings(img, known_face_locations=[face_loc])
 cv2.imshow("imagen", img)
 cv2.waitKey(0)
